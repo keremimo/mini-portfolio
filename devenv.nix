b37b5a32471ccaeb75b9978ebe037396d3255683
Kerem Kilic <code@kerem.tech>
2025-07-26 21:14:09 +0200

    First commit (For real)

--- devenv.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, config, ... }:
-{
-  languages.ruby.enable = true;
-  languages.ruby.version = "3.4.5";
-  services.postgres.enable = true;
-  services.postgres.initialScript = "CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres' SUPERUSER;";
-  services.postgres.initialDatabases = [{ name = "hello_dev"; }]; packages = [ pkgs.git pkgs.openssl pkgs.libyaml] ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.libllvm ];
-}
-
